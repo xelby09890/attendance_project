a8306114a8a6bde90ebd5b8b66d8f694ef87030e
Xelby09890 <alexis.carreos19@gmail.com>
2024-08-21 20:50:44 -0600

    version 2

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 require('dotenv').config();
+const authRoutes = require('./routes/authRoutes.js');
+const studentRoutes = require('./routes/studentRoutes.js');
 const PORT = process.env.PORT || 3000;
 
-//connect to database
+
+//Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
-.then(()=> { console.log('Connected to MongoDB Database') })
-.catch((err) => { console.log(`Error connecting to MongoDB Database: ${err}`)});
+.then(() => { console.log('Connected to MongoDB Database') })
+.catch((err) => { console.log(`Error connecting to database: ${err}`) });
 
 //View engines
 app.set('view engine', 'ejs');
@@ -16,7 +19,18 @@ app.set('views', './views');
 //Middlewares
 app.use(express.static('public'));
 
+app.use((err, res, req, next) =>{
+    console.error(err.stack);
+    res.status(500).send('Internal Server Error');
+    next();
+});
+
+//Routes
+app.use('/', authRoutes);
+app.use('/', studentRoutes);
+
+
 //Start server
-app.listen(PORT, () => {
+app.listen(PORT, () =>{
     console.log(`Connected to port ${PORT}`);
 });
